4d602dd27435b9f5cf387382519d9e898c229efa
furkrn <67959368+furkrn@users.noreply.github.com>
2025-04-17 21:08:26 +0300

    Modify the update script and systemd services for fixing the wrong effect shown when a user accessed the sddm through user switch.

--- plymouth_state_updater.sh
+++ plymouth_state_updater.sh
@@ -1,13 +1,38 @@
 #!/bin/bash
 
-if [[ $1 != "desktop" ]] && [[ $1 != "sddm" ]]; then
-    echo Supply either "desktop" or "sddm"
+status=""
+
+if [ -z "$1" ] 
+    then
+    echo "No arguments provided"
     exit 2
 fi
 
-if [[ $2 == "--no-shutdown" ]] && [[ "`systemctl is-system-running`" == "running" ]]; then
-    echo "No shutdown is pending"
+if [[ $1 == "--set" ]]; then
+    if [[ $2 != "desktop" ]] && [[ $2 != "sddm" ]]; then
+        echo Supply either "desktop" or "sddm"
+        exit 2
+    else
+        status=$2
+    fi
+fi
+
+if [[ $1 == "--update-using-sddm-session-check" ]]; then
+    if `loginctl list-sessions | awk '{print $3}' | grep -q 'sddm'`; then
+        status="sddm"
+    else
+        status="desktop"
+    fi
+fi
+
+if [[ $3 == "--assume-no-shutdown" ]] && [ "$(busctl get-property org.freedesktop.login1 /org/freedesktop/login1 org.freedesktop.login1.Manager PreparingForShutdown | awk '{print $2}')" = "true" ]; then
+    echo "Shutdown signal was available, value will not be changed!"
     exit 0
+fi
+
+if [[ $status == "" ]]; then
+    echo "Wrong arguments provided"
+    exit 2
 fi
 
 sed -i '/# START_USED_BY_SERVICE/,/# END_USED_BY_SERVICE/{ 
@@ -16,7 +41,7 @@ sed -i '/# START_USED_BY_SERVICE/,/# END_USED_BY_SERVICE/{
     } 
     r /dev/stdin
 }' /usr/share/plymouth/themes/PlymouthVista/PlymouthVista.script <<EOF
-global.OsState = "$1";
+global.OsState = "$status";
 EOF
 
-echo "Updated status to $1"
+echo "Updated status to $status"
